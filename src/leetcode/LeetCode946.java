fb9b1d736249f2244783a889c9b5175c4cb7cffe
rtxsrp <3479103351@qq.com>
2022-08-31 09:00:51 +0800

    add new question answer

--- src/leetcode/LeetCode946.java
+++ src/leetcode/LeetCode946.java
@@ -7,7 +7,7 @@ import java.util.Deque;
  * @author 86152
  */
 class Solution {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
+    public boolean validateStackSequences2(int[] pushed, int[] popped) {
         int n = pushed.length;
         Deque<Integer> deque = new ArrayDeque<>();
         int x = 0;
@@ -19,5 +19,20 @@ class Solution {
             }
         }
         return deque.isEmpty();
+    }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int n = pushed.length;
+        int[] stack = new int[pushed.length];
+        int index = -1;
+        int k = 0;
+        for(int i = 0; i < n; i ++) {
+            stack[++index] = pushed[i];
+            while(index != -1 && k < n && popped[k] == stack[index]) {
+                index --;
+                k ++;
+            }
+        }
+        return index == -1;
     }
 }
