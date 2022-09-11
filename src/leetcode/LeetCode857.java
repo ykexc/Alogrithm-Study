92a2d3947f96b4f06526d9bbed2a112a44543f59
rtxsrp <3479103351@qq.com>
2022-09-11 11:30:21 +0800

    add new question

--- src/leetcode/LeetCode857.java
+++ src/leetcode/LeetCode857.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 /**
  * @author 86152
@@ -10,7 +11,7 @@ public class LeetCode857 {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
         double ans = -1;
         int total = 0;
-        var integers = Arrays.stream(quality).boxed().toArray(Integer[]::new);
+        Integer[] integers = IntStream.range(0, quality.length).boxed().toArray(Integer[]::new);
         Arrays.sort(integers, (a, b) -> wage[a] * quality[b] - wage[b] * quality[a]);
         var heap = new PriorityQueue<Integer>((a, b) -> b - a);
         for (Integer integer : integers) {
