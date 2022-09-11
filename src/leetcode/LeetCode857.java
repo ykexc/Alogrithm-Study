69099290db5cfd7b962aab7732551892a39d2aab
rtxsrp <3479103351@qq.com>
2022-09-11 11:18:11 +0800

    add new question

---
+++ src/leetcode/LeetCode857.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @author 86152
+ */
+public class LeetCode857 {
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        double ans = -1;
+        int total = 0;
+        var integers = Arrays.stream(quality).boxed().toArray(Integer[]::new);
+        Arrays.sort(integers, (a, b) -> wage[a] * quality[b] - wage[b] * quality[a]);
+        var heap = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (Integer integer : integers) {
+            total += quality[integer];
+            heap.add(quality[integer]);
+            if (heap.size() > k) {
+                total -= heap.poll();
+            }
+            if (heap.size() == k) {
+                double cur = (double) total * wage[integer] / quality[integer];
+                ans = ans < 0 ? cur : Math.min(ans, cur);
+            }
+        }
+        return ans;
+    }
+}
