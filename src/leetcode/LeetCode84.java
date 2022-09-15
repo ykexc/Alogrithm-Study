b227a16517dbf18f27a1f46a60957dbedfae0fa0
rtxsrp <3479103351@qq.com>
2022-09-15 23:27:58 +0800

    add new question

--- src/leetcode/LeetCode84.java
+++ src/leetcode/LeetCode84.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
 
 /**
@@ -14,18 +13,21 @@ public class LeetCode84 {
         var r = new int[heights.length];
         Arrays.fill(l, -1);
         Arrays.fill(r, heights.length);
-        var stack = new ArrayDeque<Integer>();
+        int index = -1;
+        var stack1 = new int[heights.length];
         for (int i = 0; i < heights.length; i++) {
-            while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]) {
-                r[stack.pollLast()] = i;
+            while (index != -1 && heights[stack1[index]] > heights[i]) {
+                r[stack1[index--]] = i;
             }
-            stack.addLast(i);
+            stack1[++index] = i;
         }
+        stack1 = new int[heights.length];
+        index = -1;
         for (int i = heights.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]) {
-                l[stack.pollLast()] = i;
+            while (index != -1 && heights[stack1[index]] > heights[i]) {
+                l[stack1[index--]] = i;
             }
-            stack.addLast(i);
+            stack1[++index] = i;
         }
         int ans = 0;
         for (int i = 0; i < heights.length; i++) {
