5679d9c1ddd299c352e74cf9df07e354a4bd19b3
rtxsrp <3479103351@qq.com>
2022-09-18 20:53:28 +0800

    add new question

--- src/leetcode/LeetCode2013.java
+++ src/leetcode/LeetCode2013.java
@@ -8,29 +8,31 @@ import java.util.Map;
  */
 public class LeetCode2013 {
 
-    Map<Integer, Map<Integer, Integer>> row2Col = new HashMap<>();
+
+    Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
 
     public void add(int[] point) {
-        int x = point[0], y = point[1];
-        Map<Integer, Integer> col2Cnt = row2Col.getOrDefault(x, new HashMap<>());
-        col2Cnt.put(y, col2Cnt.getOrDefault(y, 0) + 1);
-        row2Col.put(x, col2Cnt);
+        Map<Integer, Integer> orDefault = map.getOrDefault(point[0], new HashMap<>());
+        orDefault.put(point[1], orDefault.getOrDefault(point[1], 0) + 1);
+        map.put(point[0], orDefault);
     }
 
     public int count(int[] point) {
-        int x = point[0], y = point[1];
+        int x = point[0];
+        int y = point[1];
         int ans = 0;
-        Map<Integer, Integer> col2Cnt = row2Col.getOrDefault(x, new HashMap<>());
-        for (int ny : col2Cnt.keySet()) {
-            if (ny == y) {
+        Map<Integer, Integer> orDefault = map.getOrDefault(x, new HashMap<>());
+        for (int yy : orDefault.keySet()) {
+            if (yy == y) {
                 continue;
             }
-            int c1 = col2Cnt.get(ny);
-            int len = y - ny;
-            int[] nums = new int[]{x + len, x - len};
-            for (int nx : nums) {
-                Map<Integer, Integer> temp = row2Col.getOrDefault(nx, new HashMap<>());
-                int c2 = temp.getOrDefault(y, 0), c3 = temp.getOrDefault(ny, 0);
+            int c1 = orDefault.get(yy);
+            int len = y - yy;
+            int[] dirs = {x + len, x - len};
+            for (int dir : dirs) {
+                Map<Integer, Integer> orDefault1 = map.getOrDefault(dir, new HashMap<>());
+                int c2 = orDefault1.getOrDefault(y, 0);
+                int c3 = orDefault1.getOrDefault(yy, 0);
                 ans += c1 * c2 * c3;
             }
         }
