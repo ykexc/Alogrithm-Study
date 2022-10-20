ac458c7586d073c813a574fcbe43f4a46090fdda
rtxsrp <3479103351@qq.com>
2022-10-20 10:50:11 +0800

    add new

---
+++ src/leetcode/LeetCode41.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class LeetCode41 {
+
+    /**
+     * 原地哈希
+     *
+     * @param nums
+     * @return
+     */
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = nums.length + 1;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int k = Math.abs(nums[i]);
+            if (k <= nums.length) {
+                nums[k - 1] = -Math.abs(nums[k - 1]);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+}
