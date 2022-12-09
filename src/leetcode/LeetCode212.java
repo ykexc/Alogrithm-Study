03bb41ee5192e31c4c148b45752e991651f1559c
rtxsrp <3479103351@qq.com>
2022-12-09 13:32:25 +0800

    add new

---
+++ src/leetcode/LeetCode212.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * 回溯
+ */
+public class LeetCode212 {
+
+
+    static class TrieNode {
+        String s;
+        TrieNode[] tns = new TrieNode[26];
+    }
+    void insert(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) p.tns[u] = new TrieNode();
+            p = p.tns[u];
+        }
+        p.s = s;
+    }
+    Set<String> set = new HashSet<>();
+    char[][] board;
+    int n, m;
+    TrieNode root = new TrieNode();
+    int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    boolean[][] vis = new boolean[15][15];
+    public List<String> findWords(char[][] _board, String[] words) {
+        board = _board;
+        m = board.length; n = board[0].length;
+        for (String w : words) insert(w);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int u = board[i][j] - 'a';
+                if (root.tns[u] != null) {
+                    vis[i][j] = true;
+                    dfs(i, j, root.tns[u]);
+                    vis[i][j] = false;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+    void dfs(int i, int j, TrieNode node) {
+        if (node.s != null) set.add(node.s);
+        for (int[] d : dirs) {
+            int dx = i + d[0], dy = j + d[1];
+            if (dx < 0 || dx >= m || dy < 0 || dy >= n) continue;
+            if (vis[dx][dy]) continue;
+            int u = board[dx][dy] - 'a';
+            if (node.tns[u] != null) {
+                vis[dx][dy] = true;
+                dfs(dx, dy, node.tns[u]);
+                vis[dx][dy] = false;
+            }
+        }
+    }
+}
